3721ffb441a83d271b8f4df5eb88bcc8bf6edb6c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-26 21:53:37 -0500

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
