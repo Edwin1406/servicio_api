379f4a9a832f23a4874c2f7405b5bc27b592bf3b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-26 22:46:52 -0500

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -32,41 +32,26 @@ ALLOWED_HOSTS = ['*']  # Solo para desarrollo, debes especificar los dominios en
 
 # CORS_ORIGIN_WHITELIST = []
 # CORS_ALLOW_ALL_ORIGINS = True
-
 INSTALLED_APPS = [
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'rest_framework',
-    'allauth',
-    'allauth.account',
-    # 'corsheaders', 
-    'allauth.socialaccount',
-    'cotizaciones',
-    
 ]
 
+...
 
 MIDDLEWARE = [
-
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        
-        # 'corsheaders.middleware.CorsMiddleware',
-        
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 
 # CORS_ORIGIN_WHITELIST = [
