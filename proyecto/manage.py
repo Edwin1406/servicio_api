6be013d4d61c2af0a2e2311264c20120512ad9df
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-27 02:12:41 -0500

    Update manage.py

--- proyecto/manage.py
+++ proyecto/manage.py
@@ -1,22 +1,38 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+import socket
+from random import randint
 
-
+def get_free_port():
+    port = None
+    while True:
+        random_port = randint(8000, 9000)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("", random_port))
+                port = random_port
+                return port
+            except:
+                pass
+                
 def main():
-    """Run administrative tasks."""
+    port = get_free_port() 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
+
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django..."
         ) from exc
-    execute_from_command_line(sys.argv)
+            
+    print(f"Corriendo en puerto {port}") 
 
+    execute_from_command_line([
+        sys.argv[0], 
+        "runserver", 
+        f"127.0.0.1:{port}"
+    ])
 
 if __name__ == '__main__':
     main()
