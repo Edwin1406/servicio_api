ca02597298297d471e209b2a686287012745f986
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-26 22:35:24 -0500

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -50,14 +50,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'allauth.account.middleware.AuthenticationMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+    
+   # Quité esta línea:
+   # 'allauth.account.middleware.AuthenticationMiddleware',
+    
     'rest_framework.authentication.SessionAuthentication',
 
 ]
