01b89f901169bfe2e1a9492d6cc5782a39a76f4b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-26 22:33:02 -0500

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AuthenticationMiddleware',
+    # 'allauth.account.middleware.AuthenticationMiddleware',
     'rest_framework.authentication.SessionAuthentication',
 
 ]
