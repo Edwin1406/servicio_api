684b4b1c0e090f532666fd7205cb5261a8977353
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-27 01:43:27 -0500

    Update manage.py

--- proyecto/manage.py
+++ proyecto/manage.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-
-import sys
-
 import os
+import socket
+import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto.settings")
 
     from django.core.management import execute_from_command_line
 
-    # Verifica si el puerto está disponible
-    port = 8002  # Cambia a tu puerto deseado
-    try:
-        from django.core.servers.basehttp import run
-        run(port=int(port))
-    except Exception as e:
-        print(f"Error al intentar ejecutar en el puerto {port}: {e}")
-        print(f"Intentando en el puerto siguiente...")
-        port += 1
-        run(port=int(port))
+    # Intenta encontrar un puerto disponible a partir de 8002
+    port = 8002
+    while True:
+        try:
+            # Intenta abrir un socket en el puerto actual
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind(("127.0.0.1", port))
+            break  # Si tiene éxito, sal de bucle
+        except OSError:
+            print(f"El puerto {port} está en uso. Intentando el siguiente puerto...")
+            port += 1
+
+    print(f"Ejecutando en el puerto {port}")
+    execute_from_command_line([sys.argv[0], "runserver", f"127.0.0.1:{port}"])
