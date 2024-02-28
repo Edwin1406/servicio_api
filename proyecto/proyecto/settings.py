23e0649a397a20bd487b883fcc2a26bf4735b82e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-02-27 20:27:25 -0500

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*ix*(7ypwcycvaumj@w04!i(qsey3#srrtj&9o=xb=cvpvam-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']  # Solo para desarrollo, debes especificar los dominios en producción.
 
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'corsheaders',
     'allauth.socialaccount',
+  
+
 ]
 
 
@@ -59,16 +61,43 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+
 ]
 
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",  # Para desarrollo local
-    "https://serviacrilico.com",
+    "https://serviacrilico.com/",
     "http://127.0.0.1:5501",  # Agrega aquí tu dominio en producción
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://serviacrilico.com",
+    # Otros dominios permitidos si es necesario
+]
 
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'proyecto.urls'
 
@@ -173,6 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
